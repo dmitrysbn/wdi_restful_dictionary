2d4facd0ea8cb643142f94cf5227489122d9eaf9
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-21 15:48:51 -0500

    Add edit and update methods and view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,4 +12,16 @@ class EntriesController < ApplicationController
 
   end
 
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+
+  end
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
+
 end
